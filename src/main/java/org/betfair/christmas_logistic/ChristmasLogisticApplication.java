00c02a725c3008586da00d5ad73cb667d45386d9
Stefan <stefan@vestinda.com>
2024-11-19 18:44:24 +0200

    Initial commit

---
+++ src/main/java/org/betfair/christmas_logistic/ChristmasLogisticApplication.java
@@ -0,0 +1,13 @@
+package org.betfair.christmas_logistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasLogisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasLogisticApplication.class, args);
+	}
+
+}
